c33cddf85ef3842d28bc7d41d7ed09edb5ea44d2
刘涛 <2863433495@qq.com>
2023-05-14 18:22:22 +0800

    修改打包报错问题

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { request } from "@/utils/request";
-import type { LoginResponse } from '@/views/login/loginType.ts'
+import type { LoginResponse } from '@/views/login/loginType'
 //获取token
 export async function loginApi(loginData: API.LoginParams): Promise<LoginResponse> {
     return request(
